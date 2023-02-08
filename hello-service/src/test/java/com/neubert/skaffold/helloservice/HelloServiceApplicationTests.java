fa94dc01540fae4a6c4230b1a327955437469f15
Tobias Neubert <tobi@s-neubert.net>
2023-02-08 10:36:43 +0100

    Simple REST with A/B test

---
+++ hello-service/src/test/java/com/neubert/skaffold/helloservice/HelloServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.neubert.skaffold.helloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
